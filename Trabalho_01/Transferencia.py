645e2a33a139af16457cae5ad61bcbbedc03ea0b
Jose Luiz <joseluizmacielp@gmail.com>
2018-05-18 23:52:15 -0300

    inteiro para bytes para transferencia

--- Trabalho_01/Transferencia.py
+++ Trabalho_01/Transferencia.py
@@ -2,6 +2,7 @@ from threading import Thread
 import socket
 import os
 
+
 class Transferencia(Thread):
     def __init__(self, num):
         Thread.__init__(self)
@@ -16,34 +17,37 @@ class Transferencia(Thread):
         procura o nome do arquivo
         :return: nome do arquivo ou 0 caso nao encontrado
         """
+        # o nome esta em formato de bytes entao foi decodificado para transforma-lo em uma string
         self.nome = self.nome.decode()
-        comandos = self.nome.split(" ")
-        print(comandos)
-        diretorio = (os.path.dirname(os.path.realpath(__file__))) + "/streamer/" + comandos[1]
-        print(diretorio)
-        try:
-            self.arq = open(diretorio, "rb")
-        except FileNotFoundError:
-            print("nao deu")
-            return None
+        arquivo = self.nome.split(" ")
+        # diretorio streamer mais o nome do arquivo digitado
+        diretorio = (os.path.dirname(os.path.realpath(__file__))) + "/streamer/" + arquivo[1]
 
-        return self.nome                       # se ok return nome do arquivo
+        try:
+            self.arq = open(diretorio, "rb") # leitura em forma de bytes
+        except FileNotFoundError:
+            return 0             # arquivo nao encontrado
+
+        return arquivo[1]                       # se ok return nome do arquivo
 
     def run(self):
         """
         Transferencia do arquivo
         :return:
         """
-        if not self.arquivo:
+        if self.arquivo == 0:
             return -1                         # enviar menssagem de erro ao cliente
 
         udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        msg = 'passar'.encode("utf-8")
+
         contamsg = 0
-        while msg != b'':
-            contamsg += 1
-            msg = contamsg.encode()
+        tamanho_msg = b'\x00\x01'
+        while int.from_bytes(tamanho_msg, 'big') != 0:
             msg = msg + self.arq.read(self.buffleitura)  # leitura de n bits do arquivo
+            tamanho_msg = len(msg).to_bytes(2, 'big')
+            contamsg = contamsg.to_bytes(2, 'big')
+            head = b''.join([contamsg, tamanho_msg])
+            msg = b''.join([head, msg])
             udp.sendto(msg, self.dest)
 
         udp.close()
